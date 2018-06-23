7ecc93680fd2588d166f34009a7cc31ef614cc00
anrosant <anrosant@espol.edu.ec>
2018-06-23 02:37:50 -0500

    Creacion del sitio web

---
+++ historialclinico/historial/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Empleado(models.Model):
+    cedula = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correo = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
+    profesion = models.CharField(max_length=50)
+    estado_civil = models.CharField(max_length=10)
+    edad = models.IntegerField()
+    sexo = models.CharField(max_length=9)
+    lugar_nacimiento = models.CharField(max_length=20)
+    fecha_nacimiento = models.DateField()
+    ocupacion_act = models.CharField(max_length=50)
+    fecha_registro = models.DateField()
+    foto = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre

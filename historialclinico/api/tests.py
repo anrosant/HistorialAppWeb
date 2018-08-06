fdeb9c3ff04dc493a42d22f845f326e8a887a952
dajocape <danijo1995@gmail.com>
2018-08-05 23:05:15 -0500

    Modificación de antecedentes laborales

--- historialclinico/api/tests.py
+++ historialclinico/api/tests.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
 
-# Create your tests here.
+# Define this after the ModelTestCase
+class ConsultaMedicaTestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        user = User.objects.get(username='historial')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+        self.consulta_medica_data = {'empleado': '1', 'examen_fisico': 'Rayos x', 'fechaConsulta': '2018-08-01',
+                                     'motivo': 'Dolor de pierna', 'prob_actual':'','revision_medica': 'Algo'}
+        self.response = self.client.post(
+            reverse('api:create'),
+            self.consulta_medica_data,
+            format="json")
+
+    def test_crear_consulta_medica(self):
+        """Test the api has bucket creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
--- historialclinico/historial/static/js/nueva_ficha_medica.js
+++ historialclinico/historial/static/js/nueva_ficha_medica.js
@@ -22,59 +22,215 @@ $(document).ready(function() {
     });
 });
 
-function agregarRiesgo(tipo) {
-    var containers_tipo_riesgo = $(".container_tipo_riesgo_" + tipo);
-    var containers_nombre_riesgo = $(".container_nombre_riesgo_" + tipo);
-    var botones_eliminar = $(".boton_eliminar_riesgo_empresa_" + tipo);
+function escogerTipo(tipo, empresa, riesgo) {
+    var tipo_r;
+    var nombre;
+    var opciones;
+    var i;
+    if(tipo === 'anterior') {
+        tipo_r = $("#tipo_riesgo_" + tipo + "_" + empresa + "_" + riesgo).val();
+        nombre = $("#nombre_riesgo_" + tipo + "_" + empresa + "_" + riesgo);
+        opciones = $("#nombre_riesgo_" + tipo + "_" +  empresa + "_" + riesgo + " option");
+        for (i = 1; i < opciones.length; i++) {
+            if ($(opciones[i]).attr("class") !== tipo_r) {
+                $(opciones[i]).attr("hidden", "");
+            } else {
+                $(opciones[i]).removeAttr("hidden");
+            }
+        }
+    } else {
+        tipo_r = $("#tipo_riesgo_" + tipo + "_" + riesgo).val();
+        nombre = $("#nombre_riesgo_" + tipo + "_" + riesgo);
+        opciones = $("#nombre_riesgo_" + tipo + "_" + riesgo + " option");
+        for (i = 1; i < opciones.length; i++) {
+            if ($(opciones[i]).attr("class") !== tipo_r) {
+                $(opciones[i]).attr("hidden", "");
+            } else {
+                $(opciones[i]).removeAttr("hidden");
+            }
+        }
+    }
+}
+
+function agregarRiesgo() {
+    var containers_tipo_riesgo = $(".container_tipo_riesgo_actual");
+    var containers_nombre_riesgo = $(".container_nombre_riesgo_actual");
+    var botones_eliminar = $(".boton_eliminar_riesgo_empresa_actual");
     if(containers_tipo_riesgo.length === 1) {
         $(botones_eliminar[0]).removeAttr("hidden");
     }
-    $(containers_tipo_riesgo[0]).clone(false, true).appendTo("#container_riesgos_" + tipo + "es");
-    $(containers_nombre_riesgo[0]).clone(false, true).appendTo("#container_riesgos_" + tipo + "es");
-    $(botones_eliminar[0]).clone(false, true).appendTo("#container_riesgos_" + tipo + "es");
-    $("#boton_mas_riesgo_empresa_" + tipo).appendTo("#container_riesgos_" + tipo + "es");
-    ordenarRiesgo(tipo);
+    $(containers_tipo_riesgo[0]).clone(false, true).appendTo("#container_riesgos_actuales");
+    $(containers_nombre_riesgo[0]).clone(false, true).appendTo("#container_riesgos_actuales");
+    restablecerCopia("#container_riesgos_actuales");
+    $(botones_eliminar[0]).clone(false, true).appendTo("#container_riesgos_actuales");
+    $("#boton_mas_riesgo_empresa_actual").appendTo("#container_riesgos_actuales");
+    ordenarRiesgo();
 }
 
-function eliminarRiesgo(tipo, numero) {
-    $("#tipo_riesgo_" + tipo + "_" + numero).parent().remove();
-    $("#nombre_riesgo_" + tipo + "_" + numero).parent().remove();
-    $("#boton_eliminar_riesgo_" + tipo + "_" + numero).parent().remove();
-    var containers_tipo_riesgo = $(".container_tipo_riesgo_" + tipo);
-    var botones_eliminar = $(".boton_eliminar_riesgo_empresa_" + tipo);
+function restablecerCopia(elemento){
+    var cont = $(elemento).children();
+    var select = $(cont[cont.length - 1]).children()[0];
+    var opci = $(select).children();
+    for(var i = 0; i < opci.length; i++) {
+        var attr1 = $(opci[i]).attr("hidden");
+        var attr2 = $(opci[i]).attr("disabled");
+        if((typeof attr1 !== typeof undefined || attr1 !== false) &&
+            (typeof attr2 === typeof undefined || attr2 === false)) {
+            $(opci[i]).removeAttr("hidden");
+        }
+    }
+}
+
+function eliminarRiesgo(numero) {
+    $("#tipo_riesgo_actual_" + numero).parent().remove();
+    $("#nombre_riesgo_actual_" + numero).parent().remove();
+    $("#boton_eliminar_riesgo_actual_" + numero).parent().remove();
+    var containers_tipo_riesgo = $(".container_tipo_riesgo_actual");
+    var botones_eliminar = $(".boton_eliminar_riesgo_empresa_actual");
     if(containers_tipo_riesgo.length === 1) {
         $(botones_eliminar[0]).attr("hidden", "");
     }
-    ordenarRiesgo(tipo);
+    ordenarRiesgo();
 }
 
-function ordenarRiesgo(tipo) {
-    var containers_tipo_riesgo = $(".container_tipo_riesgo_" + tipo);
-    var containers_nombre_riesgo = $(".container_nombre_riesgo_" + tipo);
-    var botones_eliminar = $(".boton_eliminar_riesgo_empresa_" + tipo);
+function ordenarRiesgo() {
+    var containers_tipo_riesgo = $(".container_tipo_riesgo_actual");
+    var containers_nombre_riesgo = $(".container_nombre_riesgo_actual");
+    var botones_eliminar = $(".boton_eliminar_riesgo_empresa_actual");
     for(var i = 0; i < containers_tipo_riesgo.length; i++) {
-        $($(containers_tipo_riesgo[i]).children()[0]).attr('id', 'tipo_riesgo_' + tipo + "_" + (i + 1));
-        $($(containers_tipo_riesgo[i]).children()[0]).attr('name', 'tipo_riesgo_' + tipo + "_" + (i + 1));
-        $($(containers_tipo_riesgo[i]).children()[0]).attr('onchange', 'escogerTipo(' + (i + 1) + ')');
-        $($(containers_nombre_riesgo[i]).children()[0]).attr('id', 'nombre_riesgo_' + tipo + "_" + (i + 1));
-        $($(containers_nombre_riesgo[i]).children()[0]).attr('name', 'nombre_riesgo_' + tipo + "_" + (i + 1));
-        $($(botones_eliminar[i]).children()[0]).attr('onclick', 'eliminarRiesgo(\'' + tipo + '\', ' + (i + 1) + ')');
-        $($(botones_eliminar[i]).children()[0]).attr('id', 'boton_eliminar_riesgo_' + tipo + "_" + (i + 1));
+        $($(containers_tipo_riesgo[i]).children()[0]).attr('id', 'tipo_riesgo_actual_' + (i + 1));
+        $($(containers_tipo_riesgo[i]).children()[0]).attr('name', 'tipo_riesgo_actual_' + (i + 1));
+        $($(containers_tipo_riesgo[i]).children()[0]).attr('onchange', 'escogerTipo("actual", 0, ' + (i + 1) + ')');
+        $($(containers_nombre_riesgo[i]).children()[0]).attr('id', 'nombre_riesgo_actual_' + (i + 1));
+        $($(containers_nombre_riesgo[i]).children()[0]).attr('name', 'nombre_riesgo_actual_' + (i + 1));
+        $($(botones_eliminar[i]).children()[0]).attr('onclick', 'eliminarRiesgo("actual", ' + (i + 1) + ')');
+        $($(botones_eliminar[i]).children()[0]).attr('id', 'boton_eliminar_riesgo_actual_' + (i + 1));
     }
 }
 
-function escogerTipo(tipo, numero) {
-    var tipo_r = $("#tipo_riesgo_" + tipo + "_" + numero).val();
-    var nombre = $("#nombre_riesgo_" + tipo + "_" + numero);
-    var opciones = $("#nombre_riesgo_" + tipo + "_" + numero + " option");
-    for (var i = 1; i < opciones.length; i++) {
-        if ($(opciones[i]).attr("class") !== tipo_r) {
-            $(opciones[i]).attr("hidden", "");
-            console.log(opciones[i]);
-        } else {
-            $(opciones[i]).removeAttr("hidden");
+function agregarRiesgoAnterior(empresa) {
+    var containers_tipo_riesgo = $(".container_tipo_riesgo_anterior_" + empresa);
+    var containers_nombre_riesgo = $(".container_nombre_riesgo_anterior_" + empresa);
+    var botones_eliminar = $(".boton_eliminar_riesgo_empresa_anterior_" + empresa);
+    if(containers_tipo_riesgo.length === 1) {
+        $(botones_eliminar[0]).removeAttr("hidden");
+    }
+    $(containers_tipo_riesgo[0]).clone(false, true).appendTo("#container_riesgo_anterior_" + empresa);
+    $(containers_nombre_riesgo[0]).clone(false, true).appendTo("#container_riesgo_anterior_" + empresa);
+    restablecerCopia(".container_riesgos_anteriores");
+    $(botones_eliminar[0]).clone(false, true).appendTo("#container_riesgo_anterior_" + empresa);
+    $("#boton_mas_riesgo_empresa_anterior_" + empresa).appendTo("#container_riesgo_anterior_" + empresa);
+    ordenarRiesgoAnterior();
+}
+
+function eliminarRiesgoAnterior(empresa, riesgo) {
+    $("#tipo_riesgo_anterior_" + empresa + "_" + riesgo).parent().remove();
+    $("#nombre_riesgo_anterior_" + empresa + "_" + riesgo).parent().remove();
+    $("#boton_eliminar_riesgo_anterior_" + empresa + "_" + riesgo).parent().remove();
+    var containers_tipo_riesgo = $(".container_tipo_riesgo_anterior_" + empresa);
+    var botones_eliminar = $(".boton_eliminar_riesgo_empresa_anterior_" + empresa);
+    if(containers_tipo_riesgo.length === 1) {
+        $(botones_eliminar[0]).attr("hidden", "");
+    }
+    ordenarRiesgoAnterior();
+}
+
+function ordenarRiesgoAnterior() {
+    var containers = $(".container_riesgos_anteriores");
+    for(var h = 0; h < containers.length; h++) {
+        $(containers[h]).attr('id', 'container_riesgo_anterior_' + (h + 1));
+    }
+    for(var i = 0; i < containers.length; i++) {
+        var containers_tipo_riesgo = $("#container_riesgo_anterior_" + (i + 1) + " [class*='container_tipo_riesgo_anterior_']");
+        for(var j = 0; j < containers_tipo_riesgo.length; j++) {
+            $(containers_tipo_riesgo[j]).attr('class', 'col-xl-5 col-lg-5 col-md-5 col-sm-5 container_tipo_riesgo_anterior_' + (i + 1));
+            $($(containers_tipo_riesgo[j]).children()[0]).attr('id', 'tipo_riesgo_anterior_' + (i + 1) + "_" + (j + 1));
+            $($(containers_tipo_riesgo[j]).children()[0]).attr('name', 'tipo_riesgo_anterior_' + (i + 1) + "_" + (j + 1));
+            $($(containers_tipo_riesgo[j]).children()[0]).attr('onchange', 'escogerTipo("anterior", ' + (i + 1) + ', ' + (j + 1) + ')');
+        }
+        var containers_nombre_riesgo = $("#container_riesgo_anterior_" + (i + 1) + " [class*='container_nombre_riesgo_anterior_']");
+        for(var k = 0; k < containers_nombre_riesgo.length; k++) {
+            $(containers_nombre_riesgo[k]).attr('class', 'col-xl-5 col-lg-5 col-md-5 col-sm-5 container_nombre_riesgo_anterior_' + (i + 1));
+            $($(containers_nombre_riesgo[k]).children()[0]).attr('id', 'nombre_riesgo_anterior_' + (i + 1) + "_" + (k + 1));
+            $($(containers_nombre_riesgo[k]).children()[0]).attr('name', 'nombre_riesgo_anterior_' + (i + 1) + "_" + (k + 1));
+        }
+        var botones_eliminar = $("#container_riesgo_anterior_" + (i + 1) + " [class*='boton_eliminar_riesgo_empresa_anterior_']");
+        for(var l = 0; l < botones_eliminar.length; l++) {
+            $(botones_eliminar[l]).attr('class', 'col-xl-1 col-lg-1 col-md-1 col-sm-1 boton_eliminar_riesgo_empresa_anterior_' + (i + 1));
+            $($(botones_eliminar[l]).children()[0]).attr('id', 'boton_eliminar_riesgo_anterior_' + (i + 1) + "_" + (l + 1));
+            $($(botones_eliminar[l]).children()[0]).attr('onclick', 'eliminarRiesgoAnterior(' + (i + 1) + ', ' + (l + 1) + ')');
         }
     }
+    var botones_mas = $("[id*='boton_mas_riesgo_empresa_anterior_']");
+    for(var m = 0; m < botones_mas.length; m++) {
+        $(botones_mas[m]).attr('id', 'boton_mas_riesgo_empresa_anterior_' + (m + 1));
+        $($(botones_mas[m]).children()[0]).attr('onclick', 'agregarRiesgoAnterior(' + (m + 1) + ')');
+    }
+}
+
+function agregarEmpresa() {
+    var containers_empresas_anteriores = $(".container_empresa_anterior");
+    var botones_eliminar = $(".boton_eliminar_empresa_anterior");
+    if(containers_empresas_anteriores.length === 1) {
+        $(botones_eliminar[0]).removeAttr("hidden");
+    }
+    $(containers_empresas_anteriores[0]).clone(false, false).appendTo("#container_empresas_anteriores");
+    /*var containers_empresas = $("#container_empresas_anteriores");
+    var ultima_empresa = $(containers_empresas).children()[$(containers_empresas).children().length - 1];
+    var riesgos = $(ultima_empresa).find(".container_riesgos_anteriores select").length / 2;
+    console.log("riesgos: "+ riesgos);
+    console.log(ultima_empresa);
+    //while(riesgos > 1) {
+        var empresa = $(".container_empresa_anterior").length;
+        var riesgo = riesgos;
+        console.log("numero empresas" + empresa);
+        console.log("riesgo" + riesgo);
+        $("#tipo_riesgo_anterior_" + empresa + "_" + riesgo).parent().remove();
+        console.log($("#tipo_riesgo_anterior_" + empresa + "_" + riesgo));
+        $("#nombre_riesgo_anterior_" + empresa + "_" + riesgo).parent().remove();
+        $("#boton_eliminar_riesgo_anterior_" + empresa + "_" + riesgo).parent().remove();
+        var containers_tipo_riesgo = $(".container_tipo_riesgo_anterior_" + empresa);
+        botones_eliminar = $(".boton_eliminar_riesgo_empresa_anterior_" + empresa);
+        if(containers_tipo_riesgo.length === 1) {
+            $(botones_eliminar[0]).attr("hidden", "");
+        }
+        containers_empresas = $("#container_empresas_anteriores");
+        ultima_empresa = $(containers_empresas).children()[$(containers_empresas).children().length - 1];
+        riesgos = $(ultima_empresa).find(".container_riesgos_anteriores select").length / 2;
+        console.log("riesgos: "+ riesgos);
+    console.log(ultima_empresa);
+    //}*/
+    $(botones_eliminar[0]).clone(false, true).appendTo("#container_empresas_anteriores");
+    $("#boton_mas_empresa_anterior").appendTo("#container_empresas_anteriores");
+    ordenarEmpresa();
+}
+
+function eliminarEmpresa(numero) {
+    $("#container_empresa_anterior_" + numero).remove();
+    $("#boton_eliminar_empresa_anterior_" + numero).parent().remove();
+    var containers_empresas_anteriores = $(".container_empresa_anterior");
+    var botones_eliminar = $(".boton_eliminar_empresa_anterior");
+    if(containers_empresas_anteriores.length === 1) {
+        $(botones_eliminar[0]).attr("hidden", "");
+    }
+    ordenarEmpresa();
+}
+
+function ordenarEmpresa() {
+    var containers_empresas_anteriores = $(".container_empresa_anterior");
+    var containers_nombre_empresas_anteriores = $(".container_nombre_empresa_anterior");
+    var containers_tiempo_empresas_anteriores = $(".container_tiempo_empresa_anterior");
+    var containers_cargo_empresas_anteriores = $(".container_cargo_empresa_anterior");
+    var botones_eliminar = $(".boton_eliminar_empresa_anterior");
+    for(var i = 0; i < containers_empresas_anteriores.length; i++) {
+        $($(containers_empresas_anteriores[i])).attr('id', 'container_empresa_anterior_' + (i + 1));
+        $($(containers_nombre_empresas_anteriores[i]).children()[1]).attr('id', 'nombre_empresa_anterior_' + (i + 1));
+        $($(containers_tiempo_empresas_anteriores[i]).children()[1]).attr('id', 'tiempo_empresa_anterior_' + (i + 1));
+        $($(containers_cargo_empresas_anteriores[i]).children()[1]).attr('id', 'cargo_empresa_anterior_' + (i + 1));
+        $($(botones_eliminar[i]).children()[0]).attr('onclick', 'eliminarEmpresa(' + (i + 1) + ')');
+        $($(botones_eliminar[i]).children()[0]).attr('id', 'boton_eliminar_empresa_anterior_' + (i + 1));
+    }
+    ordenarRiesgoAnterior();
 }
 
 function agregarPatologia(tipo) {
@@ -86,10 +242,10 @@ function agregarPatologia(tipo) {
     }
     if(tipo === 'familiar') {
         var containers_parentesco_patologia = $(".container_parentesco_patologia_" + tipo);
-        $(containers_parentesco_patologia[0]).clone(false, true).appendTo("#container_patologias_" + tipo + "es");
+        $(containers_parentesco_patologia[0]).clone(false, false).appendTo("#container_patologias_" + tipo + "es");
     }
-    $(containers_nombre_patologia[0]).clone(false, true).appendTo("#container_patologias_" + tipo + "es");
-    $(containers_detalle_patologia[0]).clone(false, true).appendTo("#container_patologias_" + tipo + "es");
+    $(containers_nombre_patologia[0]).clone(false, false).appendTo("#container_patologias_" + tipo + "es");
+    $(containers_detalle_patologia[0]).clone(false, false).appendTo("#container_patologias_" + tipo + "es");
     $(botones_eliminar[0]).clone(false, true).appendTo("#container_patologias_" + tipo + "es");
     $("#boton_mas_patologia_" + tipo).appendTo("#container_patologias_" + tipo + "es");
     ordenarPatologia(tipo);
@@ -138,8 +294,8 @@ function agregarVacuna(tipo) {
     if(containers_dosis_vacuna.length === 1) {
         $(botones_eliminar[0]).removeAttr("hidden");
     }
-    $(containers_dosis_vacuna[0]).clone(false, true).appendTo("#inmunizaciones_" + tipo);
-    $(containers_fecha_vacuna[0]).clone(false, true).appendTo("#inmunizaciones_" + tipo);
+    $(containers_dosis_vacuna[0]).clone(false, false).appendTo("#inmunizaciones_" + tipo);
+    $(containers_fecha_vacuna[0]).clone(false, false).appendTo("#inmunizaciones_" + tipo);
     $(botones_eliminar[0]).clone(false, true).appendTo("#inmunizaciones_" + tipo);
     $("#boton_mas_vacuna_" + tipo).appendTo("#inmunizaciones_" + tipo);
     ordenarVacuna(tipo);
@@ -178,8 +334,8 @@ function agregarAparato() {
     if(containers_nombre_aparato.length === 1) {
         $(botones_eliminar[0]).removeAttr("hidden");
     }
-    $(containers_nombre_aparato[0]).clone(false, true).appendTo("#container_aparatos_sistema");
-    $(containers_detalle_aparato[0]).clone(false, true).appendTo("#container_aparatos_sistema");
+    $(containers_nombre_aparato[0]).clone(false, false).appendTo("#container_aparatos_sistema");
+    $(containers_detalle_aparato[0]).clone(false, false).appendTo("#container_aparatos_sistema");
     $(botones_eliminar[0]).clone(false, true).appendTo("#container_aparatos_sistema");
     $("#boton_mas_aparato_sistema").appendTo("#container_aparatos_sistema");
     ordenarAparato();

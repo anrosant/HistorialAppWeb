a0d642c91734728397f9063815ed9b992cd3d1f6
anrosant <anrosant@espol.edu.ec>
2018-08-15 03:08:44 +0000

    merge

--- historialclinico/historialclinico/settings.py
+++ historialclinico/historialclinico/settings.py
@@ -101,22 +101,22 @@ WSGI_APPLICATION = 'historialclinico.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'historialMedico$historialMedico',
-#         'USER': 'historialMedico',
-#         'PASSWORD': 'm3d1c0hist',
-#         'HOST': 'historialMedico.mysql.pythonanywhere-services.com',
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+ 'default': {
+     'ENGINE': 'django.db.backends.mysql',
+     'NAME': 'historialMedico$historialMedico',
+     'USER': 'historialMedico',
+     'PASSWORD': 'm3d1c0hist',
+     'HOST': 'historialMedico.mysql.pythonanywhere-services.com',
+ }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
